8e041a1f0b1a8e657ee5aea45b299ef38fd52287
Felipe Ribeiro <piporibeiro@gmail.com>
2022-09-14 21:10:57 -0300

    feat: create Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,26 @@
+import { styles } from './styles'
+import { View, Image, FlatList } from 'react-native'
+import logImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading'
+import { GameCard } from '../../components/GameCard'
+import { GAMES } from '../../utils/games'
+
+export function Home() {
+  return (
+    <View style={styles.containers}>
+      <Image source={logImg} style={styles.logo} />
+      <Heading
+        title={'Encontre seu duo!'}
+        subtitle={'Selecione o game que deseja jogar...'}
+      />
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  )
+}
