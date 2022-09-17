669feef2fb6af7a42c769a839399b38aafc38432
Felipe Ribeiro <piporibeiro@gmail.com>
2022-09-17 15:41:38 -0300

    feat: create Duo card

---
+++ src/components/DuoCard/index.tsx
@@ -0,0 +1,44 @@
+import { Text, TouchableOpacity, View } from 'react-native'
+import { styles } from './styles'
+import { DuoInfo } from '../DuoInfo'
+import { THEME } from '../../theme'
+import { GameController } from 'phosphor-react-native'
+
+export interface DuoCardProps {
+  id: string
+  hourEnd: string
+  hourStart: string
+  name: string
+  useVoiceChannel: boolean
+  weekDays: string[]
+  yearsPlaying: number
+}
+
+interface Props {
+  data: DuoCardProps
+  onConnect: () => void
+}
+
+export function DuoCard({ data, onConnect }: Props) {
+  return (
+    <View style={styles.container}>
+      <DuoInfo label={'Nome'} value={data.name} />
+      <DuoInfo label={'Tempo de Jogo'} value={`${data.yearsPlaying} ano(s)`} />
+      <DuoInfo
+        label={'Disponibilidade'}
+        value={`${data.weekDays.length} dias \u2022 ${data.hourStart} - ${data.hourEnd}`}
+      />
+      <DuoInfo
+        label={'Chamada de áudio?'}
+        value={data.useVoiceChannel ? 'sim' : 'não'}
+        colorValue={
+          data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT
+        }
+      />
+      <TouchableOpacity style={styles.button} onPress={onConnect}>
+        <GameController color={THEME.COLORS.TEXT} size={20} />
+        <Text style={styles.buttonTitle}>Conectar</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
