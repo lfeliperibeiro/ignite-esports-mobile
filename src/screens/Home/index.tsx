a19a1e7af889998cc2444d63cf367503757a950e
Felipe Ribeiro <piporibeiro@gmail.com>
2022-09-17 13:38:15 -0300

    feat: use api data

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,10 +2,18 @@ import { styles } from './styles'
 import { View, Image, FlatList } from 'react-native'
 import logImg from '../../assets/logo-nlw-esports.png'
 import { Heading } from '../../components/Heading'
-import { GameCard } from '../../components/GameCard'
-import { GAMES } from '../../utils/games'
+import { GameCard, GameCardProps } from '../../components/GameCard'
+import { useEffect, useState } from 'react'
+import { api } from '../../lib/api'
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  useEffect(() => {
+    api.get('games').then((response) => {
+      setGames(response.data)
+    })
+  }, [])
   return (
     <View style={styles.containers}>
       <Image source={logImg} style={styles.logo} />
@@ -15,7 +23,7 @@ export function Home() {
       />
       <FlatList
         contentContainerStyle={styles.contentList}
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         horizontal
